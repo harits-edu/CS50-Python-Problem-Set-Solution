25040d13d61571a89162302f189b5af66d088667
Haritsyam Anshari <harits.edu@gmail.com>
2025-10-17 20:48:08 +0700

    CS50P

---
+++ 0-Functions-Variables/1-indoor/indoor.py
@@ -0,0 +1,11 @@
+def main():
+    input_var = input("Enter anything you want: ")
+    indoorv(input_var)
+
+    print("Thank you for entering your input!")
+
+def indoorv(what):
+    what = what.lower()
+    print(what)
+
+main()
